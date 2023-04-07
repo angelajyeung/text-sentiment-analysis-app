38d54e43bf01b96c08fe08064436f457278c34fc
Angela Yeung <angelajyeung127@gmail.com>
2023-04-07 17:20:08 -0400

    Application code for sentiment analysis

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st  #Web App
+from pysentimiento import create_analyzer 
+
+# title
+st.title("Extract Sentiment from Text")
+
+# subtitle
+st.markdown("## Sentiment Analysis - Using `streamlit` -  hosted on 🤗 Spaces")
+
+st.markdown("")
+
+# sentiment analyzer
+analyzer = create_analyzer(task="sentiment", lang="en")
+
+# text input
+text = st.text_input("Enter text here", "")
+
+if st.button("Submit"):
+    # analyze the text
+    prediction = analyzer.predict(text)
+    # print the sentiment
+    st.write(prediction)
