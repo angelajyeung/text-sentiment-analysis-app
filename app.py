8dbb6073615dd2ddf7497cebaa65c3c6185d9b92
Angela Yeung <angelajyeung127@gmail.com>
2023-04-07 18:06:31 -0400

    Update the submit button

--- app.py
+++ app.py
@@ -15,11 +15,8 @@ analyzer = create_analyzer(task="sentiment", lang="en")
 # text input
 text = st.text_input("Enter text here", "")
 
-# submit button
-button = st.button("Submit")
-
 # sentiment analysis of input text
-if button is not None:
+if st.button("Submit"):
     # analyze the text
     prediction = analyzer.predict(text)
     # print the sentiment
