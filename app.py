a10d4b60d1bbc4e9c9251c61c31e62465dd0e9ca
Angela Yeung <angelajyeung127@gmail.com>
2023-04-07 23:02:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,6 @@ text = st.text_area("Enter text here", default)
 if st.button("Submit"):
     # analyze the text
     prediction = classifier("I am happy today.")
-    preds = {"score": round(prediction["score"], 4), "label": prediction["label"]}
+    preds = [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in prediction]
     # print the sentiment
     preds
