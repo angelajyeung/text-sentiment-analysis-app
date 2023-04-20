af95abd81bfb9351add7c2c3d999b9ad6ff9612b
Angela Yeung <angelajyeung127@gmail.com>
2023-04-20 18:56:35 -0400

    Update text box

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ classifier = pipeline(task="sentiment-analysis")
 
 # text input
 default = "I am happy today."
-text = st.text_area("Enter text here", default)
+text = st.text_area("Enter text here", "")
 
 # sentiment analysis of input text
 if st.button("Submit"):
     # analyze the text
-    prediction = classifier("I am happy today.")
+    prediction = classifier(text)
     preds = [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in prediction]
     # print the sentiment
     st.write(preds)
