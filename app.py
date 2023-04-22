69327022d4159ec9f623911b1fdf6764e0011227
Angela Yeung <angelajyeung127@gmail.com>
2023-04-22 11:32:10 -0400

    Debugging

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-# import streamlit as st  #Web App
-# from transformers import pipeline
+import streamlit as st  #Web App
+from transformers import pipeline
 
-# # title
-# st.title("Extract Sentiment from Text")
+# title
+st.title("Extract Sentiment from Text")
 
-# # subtitle
-# st.markdown("## Sentiment Analysis - Using `streamlit` -  hosted on 🤗 Spaces")
+# subtitle
+st.markdown("## Sentiment Analysis - Using `streamlit` -  hosted on 🤗 Spaces")
 
-# st.markdown("")
+st.markdown("")
 
-# # sentiment analyzer
-# classifier = pipeline(task="sentiment-analysis")
+# sentiment analyzer
+classifier = pipeline(task="sentiment-analysis")
 
-# # text input
-# default = "I am happy today."
-# text = st.text_area("Enter text here", "")
+# text input
+default = "I am happy today."
+text = st.text_area("Enter text here", "")
 
-# # sentiment analysis of input text
-# if st.button("Submit"):
-#     # analyze the text
-#     prediction = classifier(text)
-#     preds = [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in prediction]
-#     # print the sentiment
-#     st.write(preds)
+# sentiment analysis of input text
+if st.button("Submit"):
+    # analyze the text
+    prediction = classifier(text)
+    preds = [{"score": round(pred["score"], 4), "label": pred["label"]} for pred in prediction]
+    # print the sentiment
+    st.write(preds)
 
-import pandas as pd
-import streamlit as st
-from transformers import pipeline, AutoTokenizer
+# import pandas as pd
+# import streamlit as st
+# from transformers import pipeline, AutoTokenizer
 
 # @st.cache(allow_output_mutation=True)
 # def load_model(model_name):
@@ -40,20 +40,20 @@ from transformers import pipeline, AutoTokenizer
 #     max_label, max_prob = max(predictions, key=lambda x: x['score'])
 #     return max_label, max_prob
 
-# title
-st.title("Text Toxicity Classifier")
+# # title
+# st.title("Text Toxicity Classifier")
 
-# subtitle
-st.markdown("## Toxicity Classifier - Using `streamlit` -  hosted on 🤗 Spaces")
+# # subtitle
+# st.markdown("## Toxicity Classifier - Using `streamlit` -  hosted on 🤗 Spaces")
 
-st.markdown("")
+# st.markdown("")
 
-models = {
-    "bert-base-uncased": "bert-base-uncased",
-    "distilbert-base-uncased": "distilbert-base-uncased",
-    "roberta-base": "roberta-base",
-    "electra-base": "google/electra-base-discriminator",
-}
+# models = {
+#     "bert-base-uncased": "bert-base-uncased",
+#     "distilbert-base-uncased": "distilbert-base-uncased",
+#     "roberta-base": "roberta-base",
+#     "electra-base": "google/electra-base-discriminator",
+# }
 
 # model_name = st.sidebar.selectbox("Select a finetuned model", list(models.keys()))
 
