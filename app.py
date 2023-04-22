0c7efcde98e13e8d3a0c4fca4517ffdf9e5178ed
Angela Yeung <angelajyeung127@gmail.com>
2023-04-22 14:57:51 -0400

    debugging

--- app.py
+++ app.py
@@ -24,73 +24,110 @@
 #     # print the sentiment
 #     st.write(preds)
 
+# import streamlit as st
+# import pandas as pd
+# from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+
+# # Load the fine-tuned model from Hugging Face Model Hub
+# @st.cache(allow_output_mutation=True)
+# def load_model(model_name):
+#     tokenizer = AutoTokenizer.from_pretrained(model_name)
+#     model = AutoModelForSequenceClassification.from_pretrained(model_name)
+#     classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+#     return classifier
+
+# # Define the labels and their corresponding colors
+# LABELS = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+# COLORS = ["#ff5733", "#ffcc33", "#cc33ff", "#33ff57", "#57b5ff", "#c733ff"]
+
+# # Define a function to extract the highest-scoring label and its probability
+# def extract_labels(predictions):
+#     scores = [p["score"] for p in predictions]
+#     max_score = max(scores)
+#     max_index = scores.index(max_score)
+#     return LABELS[max_index], max_score
+
+# # Define the Streamlit app
+# def main():
+#     # Title
+#     st.title("Toxic Comment Classification")
+
+#     # Subtitle
+#     st.markdown("## Multi-Class Classification - Using `HuggingFace` - Hosted on :hugging_face: Spaces")
+
+#     # Select a model
+#     model_name = st.selectbox("Select a fine-tuned model:", ["bert-base-uncased", "roberta-base", "distilbert-base-uncased"])
+
+#     # Load the model
+#     classifier = load_model(model_name)
+
+#     # Load the dataset
+#     data = pd.read_csv("test.csv")
+
+#     # Create a table to display the tweet and its predicted class
+#     st.write("## Test Data")
+#     st.write(data[["comment_text"]].head(10))
+
+#     # Predict the labels for each tweet
+#     predictions = classifier(data["comment_text"].tolist())
+
+#     # Create a table to display the predicted classes and their probabilities
+#     st.write("## Predicted Classes and Probabilities")
+#     for i in range(10):
+#         tweet = data["comment_text"][i]
+#         predicted_label, predicted_score = extract_labels(predictions[i])
+#         st.write(
+#             {
+#                 "Tweet": tweet,
+#                 "Predicted Class": predicted_label,
+#                 "Probability": f"{predicted_score:.2f}",
+#             },
+#             unsafe_allow_html=True,
+#         )
+
+#         # Add some color to the predicted label
+#         predicted_color = COLORS[LABELS.index(predicted_label)]
+#         st.markdown(
+#             f'<span style="color:{predicted_color}">{predicted_label}</span>',
+#             unsafe_allow_html=True,
+#         )
+
+# if __name__ == "__main__":
+#     main()
+
 import streamlit as st
 import pandas as pd
-from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-# Load the fine-tuned model from Hugging Face Model Hub
-@st.cache(allow_output_mutation=True)
-def load_model(model_name):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
-    return classifier
+# Load the finetuned models
+MODEL_NAMES = ['bert-base-uncased', 'roberta-base', 'distilbert-base-uncased']
+MODELS = {model_name: (AutoTokenizer.from_pretrained(model_name),
+                       AutoModelForSequenceClassification.from_pretrained(model_name)) for model_name in MODEL_NAMES}
 
-# Define the labels and their corresponding colors
-LABELS = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-COLORS = ["#ff5733", "#ffcc33", "#cc33ff", "#33ff57", "#57b5ff", "#c733ff"]
+# Define the function for making predictions with a given model
+def predict(model_name, text):
+    tokenizer, model = MODELS[model_name]
+    inputs = tokenizer(text, return_tensors='pt')
+    outputs = model(**inputs)
+    probs = outputs.logits.softmax(dim=1).detach().numpy()[0]
+    return probs
 
-# Define a function to extract the highest-scoring label and its probability
-def extract_labels(predictions):
-    scores = [p["score"] for p in predictions]
-    max_score = max(scores)
-    max_index = scores.index(max_score)
-    return LABELS[max_index], max_score
-
-# Define the Streamlit app
-def main():
-    # Title
-    st.title("Toxic Comment Classification")
-
-    # Subtitle
-    st.markdown("## Multi-Class Classification - Using `HuggingFace` - Hosted on :hugging_face: Spaces")
-
-    # Select a model
-    model_name = st.selectbox("Select a fine-tuned model:", ["bert-base-uncased", "roberta-base", "distilbert-base-uncased"])
-
-    # Load the model
-    classifier = load_model(model_name)
-
-    # Load the dataset
-    data = pd.read_csv("test.csv")
-
-    # Create a table to display the tweet and its predicted class
-    st.write("## Test Data")
-    st.write(data[["comment_text"]].head(10))
-
-    # Predict the labels for each tweet
-    predictions = classifier(data["comment_text"].tolist())
-
-    # Create a table to display the predicted classes and their probabilities
-    st.write("## Predicted Classes and Probabilities")
-    for i in range(10):
-        tweet = data["comment_text"][i]
-        predicted_label, predicted_score = extract_labels(predictions[i])
-        st.write(
-            {
-                "Tweet": tweet,
-                "Predicted Class": predicted_label,
-                "Probability": f"{predicted_score:.2f}",
-            },
-            unsafe_allow_html=True,
-        )
-
-        # Add some color to the predicted label
-        predicted_color = COLORS[LABELS.index(predicted_label)]
-        st.markdown(
-            f'<span style="color:{predicted_color}">{predicted_label}</span>',
-            unsafe_allow_html=True,
-        )
-
-if __name__ == "__main__":
-    main()
+# Define the app
+def app():
+    # Set the title and subtitle
+    st.title("Toxicity Classification App")
+    st.markdown("## Using `streamlit` and HuggingFace - hosted on :hugging_face: Spaces")
+    
+    # Add a dropdown menu to select the model
+    model_name = st.selectbox('Select a finetuned model', MODEL_NAMES)
+    
+    # Add a text area to input the text
+    text = st.text_area('Enter a tweet')
+    
+    # Make the prediction and show the result in a table
+    if st.button('Classify'):
+        probs = predict(model_name, text)
+        labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+        df = pd.DataFrame({'Text': [text]*len(labels), 'Class': labels, 'Probability': probs})
+        df = df.sort_values('Probability', ascending=False)
+        st.table(df[['Text', 'Class', 'Probability']])
